ca57331f977d448dbd5ae465b08a3af31650b52c
Bradley Le <brabrele@gmail.com>
2022-01-07 02:38:27 -0800

    added date and time and its auto-updater

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,18 @@
     // a function every second
         // This function will call the method that uses the current date and formats
         // using any variation of the month, day, year, and the current time of the day
+
+// Calls the function on reload to instantly load the current time
+// the setInterval does not immediately load the time by itself
+getDate();
+
+// This function uses moment.js to take the current date and time and formats it
+function getDate() {
+    // sets the format for the current date and time
+    var today = moment().format("MMMM Do, YYYY, hh:mm:ss");
+    // sets the text of the current date and time to the header
+    $("#currentDay").text(today);
+}
+
+// This will cause the page to dynamically update the time every second
+setInterval(getDate, 1000);

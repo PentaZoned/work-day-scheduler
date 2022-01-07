9fa61bcae3513bac8f151dbbdd62afdb2f0b4955
Bradley Le <brabrele@gmail.com>
2022-01-07 02:20:42 -0800

    added pseudocode for the date and time function

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// The current date and time should display on the top of the page on reload
+    // use moment.().format() to add the current date to the header
+    // To dynamically update the date, I will create an interval that will trigger
+    // a function every second
+        // This function will call the method that uses the current date and formats
+        // using any variation of the month, day, year, and the current time of the day

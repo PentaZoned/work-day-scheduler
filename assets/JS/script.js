a591dcc08682a51682d855a004eb5fa8c2bd1d92
Bradley Le <brabrele@gmail.com>
2022-01-07 16:15:13 -0800

    corrected current hour coloring, simplified adding and removing color classes, comments add

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -32,34 +32,52 @@ var twoPM = moment().hour(14).minute(00).second(00);
 var threePM = moment().hour(15).minute(00).second(00);
 var fourPM = moment().hour(16).minute(00).second(00);
 var fivePM = moment().hour(17).minute(00).second(00);
+// sixPM variable is used to compare the fivePM var
+// Will not be compared in the for loop
+var sixPM = moment().hour(18).minute(00).second(00);
 
 // array is used to hold the hours to iterate the checkTime function below
+// sixPM is not included in the for loop
 var hoursArray = [nineAM, tenAM, elevenAM, twelveAM, onePM,
-                 twoPM, threePM, fourPM, fivePM];
+                 twoPM, threePM, fourPM, fivePM, sixPM];
 
 // Function is used to check the current time and compare it with the hours of a
 // regular work schedule 9 AM - 5 PM
 function checkTime() {
     
     // iterate through each specific hour
-    for(var i = 0; i < hoursArray.length; i++) {
+    for(var i = 0; i < hoursArray.length - 1; i++) {
         // checks if the current time is before the specific hour of today
         var isFuture = moment().isBefore(hoursArray[i]);
-        var isPast = moment().isAfter(hoursArray[i]);
+        // checks if the current time is after the hour at the next index
+        var isPast = moment().isAfter(hoursArray[i+1]);
         // just to check if there are any errors
         console.log(isFuture);
         console.log(isPast);
 
-        // comparison check to determine how the hours are color-coded based on time
+        // comparison check to determine how the hours are color-coded based on time'
+        // If it is not in the future and not in the past, it is in the present hour
         if(isFuture === false && isPast === false) {
-            // colors the specific hour gray if it is the current hour
-            containerEl.children().eq(i).children().eq(1).addClass("present");
+
+            // colors the specific hour red if it is the current hour
+            // removes future and past classes if it exists for this element
+            // adds present class
+            containerEl.children().eq(i).children().eq(1).removeClass("future past").addClass("present");
+
         } else if (isFuture === true) {
+
             // colors the specific hour green if it has yet to pass
-            containerEl.children().eq(i).children().eq(1).addClass("future");
+            // removes present and past classes if it exists for this element
+            // adds future class
+            containerEl.children().eq(i).children().eq(1).removeClass("present past").addClass("future");
+
         } if (isPast === true) {
-            // colors the specific hour red if the time has already passed
-            containerEl.children().eq(i).children().eq(1).addClass("past");
+
+            // colors the specific hour gray if the time has already passed
+            // removes future and present classes if it exists for this element
+            // adds past class
+            containerEl.children().eq(i).children().eq(1).removeClass("future present").addClass("past");
+
         }
     }
 }
